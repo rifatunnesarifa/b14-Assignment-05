b284d264c8f0e43b7bec1bc9d0555ed638f05daf
starryfisho <rifatunnesarifa02@gmail.com>
2026-09-13 20:29:21 +0600

    doing tech cards

--- devStack/src/components/TechCards.tsx
+++ devStack/src/components/TechCards.tsx
@@ -1,7 +1,33 @@
-export default function TechCards() {
+import "./TechCards.css";
+import type { Technology } from "../types/Technology";
+
+type TechCardProps = {
+  technology: Technology;
+};
+export default function TechCards({ technology }: TechCardProps) {
+
   return (
-    <section>
-      
-    </section>
+    <div className="tech-card">
+      <img
+  className="tech-icon"
+  src={technology.icon}
+  alt={technology.name}
+/>
+
+      <span className="tech-badge">{technology.badge}</span>
+
+      <h3>{technology.name}</h3>
+
+      <p>{technology.description}</p>
+
+      <span>{technology.category}</span>
+
+      <p>{technology.difficulty}</p>
+
+      <p>⭐ {technology.rating}</p>
+
+      <button>Add to Stack</button>
+    </div>
   );
+
 }
--- devStack/src/components/Technologies.tsx
+++ devStack/src/components/Technologies.tsx
@@ -1,7 +1,36 @@
+import "./Technologies.css";
+import TechCards from "./TechCards";
+import { useEffect, useState } from "react";
+import type { Technology } from "../types/Technology";
+
 export default function Technologies() {
+  const [technologies, setTechnologies] = useState<Technology[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("/src/data/technologies.json")
+      .then((response) => response.json())
+      .then((data) => {
+        setTechnologies(data);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <section>
-      
+      <h2>Explore the Technologies</h2>
+          <div className="tech-grid">
+      {technologies.map((technology) => (
+        <TechCards
+          key={technology.id}
+          technology={technology}
+        />
+      ))}
+    </div>
     </section>
   );
 }
